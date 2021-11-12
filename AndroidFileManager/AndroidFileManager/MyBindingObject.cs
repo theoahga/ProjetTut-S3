dda654a45b0028b436ed2436a60453cca76ef052
theoahga <clere.theo@gmail.com>
2021-11-12 14:22:04 +0100

    ok

---
+++ AndroidFileManager/AndroidFileManager/MyBindingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AndroidFileManager
+{
+    public class MyBindingObject
+    {
+        public MyBindingObject()
+        {
+            listFilesDoc = new ObservableCollection<string>();
+
+            listFilesDoc.Add("dede");
+            listFilesDoc.Add("dede");
+            listFilesDoc.Add("dede");
+            listFilesDoc.Add("dede");
+            listFilesDoc.Add("dede");
+            listFilesDoc.Add("dede");
+        }
+
+        private ObservableCollection<string> listFilesDoc { get; set; }
+    }
+}
